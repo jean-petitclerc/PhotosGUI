867308d507f763df8958c2fc69a4be2cf73a00a5
Jean Petitclerc <jean.petitclerc@groupepp.com>
2015-07-19 22:28:21 -0400

    File menu, status bar, menu bar 2eme essaie

--- PhotosGUI.py
+++ PhotosGUI.py
@@ -2,7 +2,7 @@ __author__ = 'jean'
 
 import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QAction, QToolTip, QPushButton, QMessageBox, QDesktopWidget,\
-    qApp, QHBoxLayout, QVBoxLayout, QMenuBar, QStatusBar, QToolBar
+    qApp, QHBoxLayout, QVBoxLayout, QGridLayout, QMenuBar, QStatusBar, QToolBar, QLabel, QFrame, QSizePolicy
 from PyQt5.QtGui import QIcon, QFont
 from PyQt5.QtCore import QCoreApplication
 
@@ -14,18 +14,18 @@ class PhotosUI(QWidget):
     def initUI(self):
         QToolTip.setFont(QFont('SansSerif', 10))
         self.setToolTip('This is a <b>QWidget</b> widget.')
-        quitBtn = QPushButton('Quit', self)
+        #quitBtn = QPushButton('Quit', self)
         #quitBtn.clicked.connect(self.quitBtnEvent())
-        quitBtn.clicked.connect(QCoreApplication.instance().quit)
-        quitBtn.setToolTip('This is a <b>QPushButton</b> widget.')
-        quitBtn.resize(quitBtn.sizeHint())
+        #quitBtn.clicked.connect(QCoreApplication.instance().quit)
+        #quitBtn.setToolTip('This is a <b>QPushButton</b> widget.')
+        #quitBtn.resize(quitBtn.sizeHint())
 
-        exitAction = QAction(QIcon(None), '&Exit', self)
+        exitAction = QAction(QIcon('application-exit-4.png'), '&Exit', self)
         exitAction.setShortcut('Alt+F4')
         exitAction.setStatusTip('Exit Application')
         exitAction.triggered.connect(qApp.quit)
 
-        menuBar = QMenuBar(self)
+        menuBar = QMenuBar()
         fileMenu = menuBar.addMenu('&File')
         fileMenu.addAction(exitAction)
         fileMenu.resize(fileMenu.sizeHint())
@@ -33,22 +33,39 @@ class PhotosUI(QWidget):
         toolBar = QToolBar(self)
         toolBar.addAction(exitAction)
         #toolBar.resize(toolBar.sizeHint())
-        toolBar.setAttribute()
-        toolBar.setFixedHeight(50)
+        toolBar.setFixedHeight(60)
+
+        hozLine = QFrame()
+        hozLine.setFrameStyle(QFrame.HLine)
+        #hozLine.setSizePolicy(QSizePolicy.Minimum,QSizePolicy.Expanding)
 
         statusBar = QStatusBar(self)
         statusBar.showMessage('Ready')
 
+        grid = QGridLayout()
+        lbl_1 = QLabel('1,1')
+        lbl_2 = QLabel('1,2')
+        lbl_3 = QLabel('2,1')
+        lbl_4 = QLabel('2,2')
+        grid.addWidget(lbl_1, 1, 1)
+        grid.addWidget(lbl_2, 1, 2)
+        grid.addWidget(lbl_3, 2, 1)
+        grid.addWidget(lbl_4, 2, 2)
+
         vbox = QVBoxLayout()
+        vbox.addWidget(menuBar)
+        vbox.addWidget(hozLine)
         vbox.addWidget(toolBar)
+        # vbox.addWidget(hozLine)
+        vbox.addLayout(grid)
         vbox.addStretch(1)
         vbox.addWidget(statusBar)
 
         self.setLayout(vbox)
 
-        self.setGeometry(300, 300, 300, 220)
+        self.setGeometry(300, 300, 500, 500)
         self.setWindowTitle('Photos')
-        self.setWindowIcon(QIcon('camera_32.png'))
+        self.setWindowIcon(QIcon('camera-photo-5.png'))
         self.center()
         self.show()
 
